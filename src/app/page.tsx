faad7913592b6f050dc2604f2ebefac82e16f808
MrEnergetico <114943713+MrEnergetico@users.noreply.github.com>
2024-09-03 22:14:01 +0200

    test image react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-'use client';
+import { Image } from "@mantine/core"
 
-import { useState } from 'react';
-import { Chip } from '@mantine/core';
-
-export default function Demo() {
-  const [checked, setChecked] = useState(false);
-
+export default async function page() {
+  
   return (
-    <Chip checked={checked} onChange={() => setChecked((v) => !v)}>
-      My chip
-    </Chip>
-  );
+    <div>
+    <Image style={{height: "150px", width: "150px", marginTop:"45px", borderRadius: "20px", marginLeft: "20px"}} src="https://cdn.discordapp.com/attachments/1280620624300998676/1280620656932946132/f5340eef7ee482a208ac0d57c40cd270.png?ex=66d8be6d&is=66d76ced&hm=e0f02ef7e97620440f1619b2e73a88b88b83c31b12b5cae9decc5982ed4cf55b&"></Image>
+    
+    </div>
+  )
 }
