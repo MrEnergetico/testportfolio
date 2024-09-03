07bb2337b5d0d05428b3333651d0ce81ecdc3832
MrEnergetico <114943713+MrEnergetico@users.noreply.github.com>
2024-09-03 22:19:02 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function page() {
   
   return (
     <div>
-    <Image style={{height: "150px", width: "150px", marginTop:"45px", borderRadius: "20px", marginLeft: "20px"}} src="https://cdn.discordapp.com/attachments/1280620624300998676/1280620656932946132/f5340eef7ee482a208ac0d57c40cd270.png?ex=66d8be6d&is=66d76ced&hm=e0f02ef7e97620440f1619b2e73a88b88b83c31b12b5cae9decc5982ed4cf55b&"></Image>
+    <Image style={{height: "500px", width: "500px", marginTop:"45px", borderRadius: "20px", marginLeft: "20px"}} src="https://cdn.discordapp.com/attachments/1280620624300998676/1280620656932946132/f5340eef7ee482a208ac0d57c40cd270.png?ex=66d8be6d&is=66d76ced&hm=e0f02ef7e97620440f1619b2e73a88b88b83c31b12b5cae9decc5982ed4cf55b&"></Image>
     
     </div>
   )
